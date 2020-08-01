7068cdebfee0f9fe965ed88d986b458eccf9e573
Michael Stoyanov <maio246@gmail.com>
2020-08-01 17:36:47 +0300

    Add files via upload
    Beta version of the notificator.

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿namespace BatteryNotificator
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Threading;
+
+    // Program class with main entry point to display an example.
+    public class Program
+    {
+        public static void Main()
+        {
+            PowerState state;
+
+            while (true)
+            {
+                state = PowerState.GetPowerState();
+
+                if (state.BatteryLifePercent <= 35 && !state.ACLineStatus.Equals(ACLineStatus.Online))
+                    for (int i = 0; i < 5; i++)
+                    {
+                        state = PowerState.GetPowerState();
+
+                        if (state.ACLineStatus.Equals(ACLineStatus.Online))
+                            break;
+
+                        System.Media.SystemSounds.Beep.Play();
+                    }
+                else if (state.BatteryLifePercent >= 95 && !state.ACLineStatus.Equals(ACLineStatus.Online))
+                    for (int i = 0; i < 5; i++)
+                    {
+                        state = PowerState.GetPowerState();
+
+                        if (state.ACLineStatus.Equals(ACLineStatus.Offline))
+                            break;
+
+                        System.Media.SystemSounds.Beep.Play();
+                    }
+
+                Thread.Sleep(TimeSpan.FromMinutes(1));
+            }
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public class PowerState
+    {
+        public ACLineStatus ACLineStatus;
+        public BatteryFlag BatteryFlag;
+        public Byte BatteryLifePercent;
+        public Byte Reserved1;
+        public Int32 BatteryLifeTime;
+        public Int32 BatteryFullLifeTime;
+
+        // direct instantation not intended, use GetPowerState.
+        private PowerState() { }
+
+        public static PowerState GetPowerState()
+        {
+            PowerState state = new PowerState();
+            if (GetSystemPowerStatusRef(state))
+                return state;
+
+            throw new ApplicationException("Unable to get power state");
+        }
+
+        [DllImport("Kernel32", EntryPoint = "GetSystemPowerStatus")]
+        private static extern bool GetSystemPowerStatusRef(PowerState sps);
+    }
+
+    // Note: Underlying type of byte to match Win32 header
+    public enum ACLineStatus : byte
+    {
+        Offline = 0, Online = 1, Unknown = 255
+    }
+
+    public enum BatteryFlag : byte
+    {
+        High = 1, Low = 2, Critical = 4, Charging = 9,
+        NoSystemBattery = 128, Unknown = 255
+    }
+
+}
